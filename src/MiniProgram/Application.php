61c1142ccb7ef20e6b17457fdaaef683044a92ba
liuml <723164417@qq.com>
2019-08-23 09:35:31 +0800

    修改文件夹大小写

---
+++ src/MiniProgram/Application.php
@@ -0,0 +1,61 @@
+<?php
+/*
+ * This file is part of the wannanbigpig/alipay-sdk-php.
+ *
+ * (c) wannanbigpig <liuml0211@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace EasyAlipay\MiniProgram;
+
+use EasyAlipay\Kernel\ServiceContainer;
+
+/**
+ * Class Application
+ *
+ * @author   liuml  <liumenglei0211@163.com>
+ * @DateTime 2019-07-18  16:13
+ *
+ * @property \EasyAlipay\MiniProgram\Members\Client $members
+ * @property \EasyAlipay\MiniProgram\QrCode\Client  $qrCode
+ * @property \EasyAlipay\MiniProgram\Version\Client $version
+ *
+ * @method mixed getBaseInfo()
+ * @method mixed updateBaseInfo(array $params)
+ * @method mixed getUsageTemplateList(array $params)
+ * @method mixed createSafeDomain(string $safeDomain)
+ * @method mixed deleteSafeDomain(string $safeDomain)
+ * @method mixed contentRiskDetect(string $content)
+ * @method mixed getCategoryList()
+ * @method mixed faceAuthenticationResultsQuery()
+ */
+class Application extends ServiceContainer
+{
+
+    /**
+     * @var array
+     */
+    protected $providers = [
+        'base' => Base\Client::class,
+        'members' => Base\Client::class,
+        'qrCode' => Base\Client::class,
+        'version' => Base\Client::class,
+        'auth' => \EasyAlipay\BaseService\Auth\Client::class,
+        'user' => \EasyAlipay\BaseService\User\Client::class,
+    ];
+
+    /**
+     * __call.
+     *
+     * @param $name
+     * @param $arguments
+     *
+     * @return mixed
+     */
+    public function __call($name, $arguments)
+    {
+        return call_user_func_array([$this['base'], $name], $arguments);
+    }
+}
---
+++ src/MiniProgram/Version/Client.php
@@ -0,0 +1,364 @@
+<?php
+/*
+ * This file is part of the wannanbigpig/alipay-sdk-php.
+ *
+ * (c) wannanbigpig <liuml0211@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace EasyAlipay\MiniProgram\Version;
+
+use EasyAlipay\Kernel\Support\Support;
+
+class Client extends Support
+{
+    /**
+     * alipay.open.mini.version.gray.cancel(小程序结束灰度).
+     *
+     * @param string $appVersion
+     *
+     * @return array|object|\Psr\Http\Message\ResponseInterface|\WannanBigPig\Supports\Collection|\WannanBigPig\Supports\Http\Response
+     *
+     * @throws \EasyAlipay\Kernel\Exceptions\InvalidSignException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \WannanBigPig\Supports\Exceptions\InvalidArgumentException
+     */
+    public function grayCancel(string $appVersion)
+    {
+        $method = 'alipay.open.mini.version.gray.cancel';
+        $params = [
+            'app_version' => $appVersion,
+        ];
+
+        return $this->request($method, [
+            'biz_content' => $params,
+        ]);
+    }
+
+    /**
+     * alipay.open.mini.version.delete(小程序删除版本).
+     *
+     * @param string $appVersion
+     *
+     * @return array|object|\Psr\Http\Message\ResponseInterface|\WannanBigPig\Supports\Collection|\WannanBigPig\Supports\Http\Response
+     *
+     * @throws \EasyAlipay\Kernel\Exceptions\InvalidSignException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \WannanBigPig\Supports\Exceptions\InvalidArgumentException
+     */
+    public function delete(string $appVersion)
+    {
+        $method = 'alipay.open.mini.version.delete';
+        $params = [
+            'app_version' => $appVersion,
+        ];
+
+        return $this->request($method, [
+            'biz_content' => $params,
+        ]);
+    }
+
+    /**
+     * alipay.open.mini.version.upload(小程序基于模板上传版本).
+     *
+     * @param string      $templateId
+     * @param string      $appVersion
+     * @param string|null $templateVersion
+     * @param string|null $ext
+     *
+     * @return array|object|\Psr\Http\Message\ResponseInterface|\WannanBigPig\Supports\Collection|\WannanBigPig\Supports\Http\Response
+     *
+     * @throws \EasyAlipay\Kernel\Exceptions\InvalidSignException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \WannanBigPig\Supports\Exceptions\InvalidArgumentException
+     */
+    public function upload(string $templateId, string $appVersion, string $templateVersion = null, string $ext = null)
+    {
+        $method = 'alipay.open.mini.version.upload';
+        $params = array_filter([
+            'template_version' => $templateVersion,
+            'ext' => $ext,
+            'template_id' => $templateId,
+            'app_version' => $appVersion,
+        ], function ($value) {
+            return !($this->checkEmpty($value));
+        });
+
+        return $this->request($method, [
+            'biz_content' => $params,
+        ]);
+    }
+
+    /**
+     * alipay.open.mini.version.gray.online(小程序灰度上架).
+     *
+     * @param string $appVersion
+     * @param string $grayStrategy
+     *
+     * @return array|object|\Psr\Http\Message\ResponseInterface|\WannanBigPig\Supports\Collection|\WannanBigPig\Supports\Http\Response
+     *
+     * @throws \EasyAlipay\Kernel\Exceptions\InvalidSignException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \WannanBigPig\Supports\Exceptions\InvalidArgumentException
+     */
+    public function grayOnline(string $appVersion, string $grayStrategy)
+    {
+        $method = 'alipay.open.mini.version.delete';
+        $params = [
+            'app_version' => $appVersion,
+            'gray_strategy' => $grayStrategy,
+        ];
+
+        return $this->request($method, [
+            'biz_content' => $params,
+        ]);
+    }
+
+    /**
+     * alipay.open.mini.version.build.query(小程序查询版本构建状态).
+     *
+     * @param string $appVersion
+     *
+     * @return array|object|\Psr\Http\Message\ResponseInterface|\WannanBigPig\Supports\Collection|\WannanBigPig\Supports\Http\Response
+     *
+     * @throws \EasyAlipay\Kernel\Exceptions\InvalidSignException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \WannanBigPig\Supports\Exceptions\InvalidArgumentException
+     */
+    public function buildQuery(string $appVersion)
+    {
+        $method = 'alipay.open.mini.version.build.query';
+        $params = [
+            'app_version' => $appVersion,
+        ];
+
+        return $this->request($method, [
+            'biz_content' => $params,
+        ]);
+    }
+
+    /**
+     * alipay.open.mini.version.audited.cancel(小程序退回开发).
+     *
+     * @param string $appVersion
+     *
+     * @return array|object|\Psr\Http\Message\ResponseInterface|\WannanBigPig\Supports\Collection|\WannanBigPig\Supports\Http\Response
+     *
+     * @throws \EasyAlipay\Kernel\Exceptions\InvalidSignException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \WannanBigPig\Supports\Exceptions\InvalidArgumentException
+     */
+    public function cancelAudited(string $appVersion)
+    {
+        $method = 'alipay.open.mini.version.audited.cancel';
+        $params = [
+            'app_version' => $appVersion,
+        ];
+
+        return $this->request($method, [
+            'biz_content' => $params,
+        ]);
+    }
+
+    /**
+     * alipay.open.mini.version.offline(小程序下架).
+     *
+     * @param string $appVersion
+     *
+     * @return array|object|\Psr\Http\Message\ResponseInterface|\WannanBigPig\Supports\Collection|\WannanBigPig\Supports\Http\Response
+     *
+     * @throws \EasyAlipay\Kernel\Exceptions\InvalidSignException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \WannanBigPig\Supports\Exceptions\InvalidArgumentException
+     */
+    public function offline(string $appVersion)
+    {
+        $method = 'alipay.open.mini.version.offline';
+        $params = [
+            'app_version' => $appVersion,
+        ];
+
+        return $this->request($method, [
+            'biz_content' => $params,
+        ]);
+    }
+
+    /**
+     * alipay.open.mini.version.list.query(小程序版本列表查询).
+     *
+     * @return array|object|\Psr\Http\Message\ResponseInterface|\WannanBigPig\Supports\Collection|\WannanBigPig\Supports\Http\Response
+     *
+     * @throws \EasyAlipay\Kernel\Exceptions\InvalidSignException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \WannanBigPig\Supports\Exceptions\InvalidArgumentException
+     */
+    public function getList()
+    {
+        $method = 'alipay.open.mini.version.list.query';
+        $params = [];
+
+        return $this->request($method, [
+            'biz_content' => $params,
+        ]);
+    }
+
+    /**
+     * alipay.open.mini.experience.create(小程序生成体验版).
+     *
+     * @param string $appVersion
+     *
+     * @return array|object|\Psr\Http\Message\ResponseInterface|\WannanBigPig\Supports\Collection|\WannanBigPig\Supports\Http\Response
+     *
+     * @throws \EasyAlipay\Kernel\Exceptions\InvalidSignException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \WannanBigPig\Supports\Exceptions\InvalidArgumentException
+     */
+    public function createExperience(string $appVersion)
+    {
+        $method = 'alipay.open.mini.experience.create';
+        $params = [
+            'app_version' => $appVersion,
+        ];
+
+        return $this->request($method, [
+            'biz_content' => $params,
+        ]);
+    }
+
+    /**
+     * alipay.open.mini.experience.cancel(小程序取消体验版).
+     *
+     * @param string $appVersion
+     *
+     * @return array|object|\Psr\Http\Message\ResponseInterface|\WannanBigPig\Supports\Collection|\WannanBigPig\Supports\Http\Response
+     *
+     * @throws \EasyAlipay\Kernel\Exceptions\InvalidSignException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \WannanBigPig\Supports\Exceptions\InvalidArgumentException
+     */
+    public function cancelExperience(string $appVersion)
+    {
+        $method = 'alipay.open.mini.experience.cancel';
+        $params = [
+            'app_version' => $appVersion,
+        ];
+
+        return $this->request($method, [
+            'biz_content' => $params,
+        ]);
+    }
+
+    /**
+     * alipay.open.mini.experience.query(小程序体验版状态查询接口).
+     *
+     * @param string $appVersion
+     *
+     * @return array|object|\Psr\Http\Message\ResponseInterface|\WannanBigPig\Supports\Collection|\WannanBigPig\Supports\Http\Response
+     *
+     * @throws \EasyAlipay\Kernel\Exceptions\InvalidSignException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \WannanBigPig\Supports\Exceptions\InvalidArgumentException
+     */
+    public function queryExperience(string $appVersion)
+    {
+        $method = 'alipay.open.mini.experience.query';
+        $params = [
+            'app_version' => $appVersion,
+        ];
+
+        return $this->request($method, [
+            'biz_content' => $params,
+        ]);
+    }
+
+    /**
+     * alipay.open.mini.version.online(小程序上架).
+     *
+     * @param string $appVersion
+     *
+     * @return array|object|\Psr\Http\Message\ResponseInterface|\WannanBigPig\Supports\Collection|\WannanBigPig\Supports\Http\Response
+     *
+     * @throws \EasyAlipay\Kernel\Exceptions\InvalidSignException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \WannanBigPig\Supports\Exceptions\InvalidArgumentException
+     */
+    public function online(string $appVersion)
+    {
+        $method = 'alipay.open.mini.version.online';
+        $params = [
+            'app_version' => $appVersion,
+        ];
+
+        return $this->request($method, [
+            'biz_content' => $params,
+        ]);
+    }
+
+    /**
+     * alipay.open.mini.version.detail.query(小程序版本详情查询).
+     *
+     * @param string $appVersion
+     *
+     * @return array|object|\Psr\Http\Message\ResponseInterface|\WannanBigPig\Supports\Collection|\WannanBigPig\Supports\Http\Response
+     *
+     * @throws \EasyAlipay\Kernel\Exceptions\InvalidSignException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \WannanBigPig\Supports\Exceptions\InvalidArgumentException
+     */
+    public function getDetail(string $appVersion)
+    {
+        $method = 'alipay.open.mini.version.detail.query';
+        $params = [
+            'app_version' => $appVersion,
+        ];
+
+        return $this->request($method, [
+            'biz_content' => $params,
+        ]);
+    }
+
+    /**
+     * alipay.open.mini.version.rollback(小程序回滚).
+     *
+     * @param string $appVersion
+     *
+     * @return array|object|\Psr\Http\Message\ResponseInterface|\WannanBigPig\Supports\Collection|\WannanBigPig\Supports\Http\Response
+     *
+     * @throws \EasyAlipay\Kernel\Exceptions\InvalidSignException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \WannanBigPig\Supports\Exceptions\InvalidArgumentException
+     */
+    public function rollback(string $appVersion)
+    {
+        $method = 'alipay.open.mini.version.rollback';
+        $params = [
+            'app_version' => $appVersion,
+        ];
+
+        return $this->request($method, [
+            'biz_content' => $params,
+        ]);
+    }
+
+    /**
+     * alipay.open.mini.version.audit.apply(小程序提交审核).
+     *
+     * @param array $params
+     *
+     * @return array|object|\Psr\Http\Message\ResponseInterface|\WannanBigPig\Supports\Collection|\WannanBigPig\Supports\Http\Response
+     *
+     * @throws \EasyAlipay\Kernel\Exceptions\InvalidSignException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \WannanBigPig\Supports\Exceptions\InvalidArgumentException
+     */
+    public function submitAudit(array $params)
+    {
+        $method = 'alipay.open.mini.version.rollback';
+        
+        return $this->request($method, [
+            'biz_content' => $params,
+        ]);
+    }
+}
